43b724a513715f12d6c53b162c3ebf8325d049cc
Denver Fernandes <sub367@gmail.com>
2016-03-23 19:00:15 +0000

    Finish support for editing animals

--- admin/edit.php
+++ admin/edit.php
@@ -1,5 +1,115 @@
 <?php
 require('../app/start.php');
+session_start();
+if(!isset($_SESSION["id"]) || !isset($_SESSION["admin"])) {
+	echo "<h1>403 Forbidden</h1>";
+	exit();
+}
+
+if(!empty($_POST)) {
+	$name = $_POST['name'];
+	$date = $_POST['date'];
+	$type = $_POST['type'];
+	$desc = $_POST['desc'];
+	$breed = $_POST['breed'];
+	$id = $_POST['id'];
+
+	$animal = array(
+		"name" => $name,
+		"dateofbirth" => $date,
+		"type" => $type,
+		"description" => $desc,
+		"breed" => $breed,
+		"animalID" => $id,
+		);
+
+	$title = "Editing " . escape($animal['name']);
+
+	if(check_empty($name) || check_empty($date) || check_empty($type) || check_empty($desc)) {
+		$errors[] = "Please ensure you have filled in all the form values.";
+	}
+
+	if(check_empty($breed)) {
+		$breed = "N/A";
+	}
+	
+	$formatted_date = new DateTime($date);
+	$current_date = new DateTime();
+	$current_date->format("Y-m-d");
+	if($formatted_date > $current_date) {
+		$errors[] = "Please ensure you have entered a date of birth that is not in the future.";
+	} 
+
+	//File stuff
+	$file_changed = false;
+	$allowed_types = array('png', 'jpg', 'gif');
+	$file_name = $_FILES["photo"]["name"];
+	$file_name_tmp = $_FILES["photo"]["tmp_name"];
+	$file_type = $_FILES["photo"]["type"];
+	$extention = pathinfo($file_name, PATHINFO_EXTENSION);
+	$encoded_file = md5($file_name).mt_rand();
+	if(in_array($extention, $allowed_types)) {
+		move_uploaded_file($file_name_tmp, "../images/".$encoded_file.".".$extention);
+		$file_changed = true;
+	} 
+
+	if(isset($errors) && count($errors) > 0) {
+		require(VIEW_ROOT . 'admin/edit.php');
+		exit();
+	}
+
+	if($file_changed) {
+		$updateAnimal = $db->prepare("
+			UPDATE animal 
+			SET
+			name = :name,
+			dateofbirth = :date,
+			description = :desc,
+			photo = :photo,
+			type = :type,
+			breed = :breed
+			WHERE animalID = :id
+		");
+	
+		$updateAnimal->execute([
+			'name' => $name,
+			'date' => $date,
+			'type' => $type,
+			'desc' => $desc,
+			'breed' => $breed,
+			'photo' => $encoded_file.".".$extention,
+			'id' => $id,
+			]);		
+		if($updateAnimal) {
+			die('Sucessfully edited '. escape($name));
+		}
+	} else {
+		$updateAnimal = $db->prepare("
+			UPDATE animal 
+			SET
+			name = :name,
+			dateofbirth = :date,
+			description = :desc,
+			type = :type,
+			breed = :breed
+			WHERE animalID = :id
+		");
+	
+		$updateAnimal->execute([
+			'name' => $name,
+			'date' => $date,
+			'type' => $type,
+			'desc' => $desc,
+			'breed' => $breed,
+			'id' => $id,
+			]);
+		if($updateAnimal) {
+			die('Sucessfully edited '. escape($name));
+		}
+	}
+
+	
+}
 if(isset($_GET['id'])) {
 
 	$id = $_GET['id'];
--- app/views/pages/admin/edit.php
+++ app/views/pages/admin/edit.php
@@ -1,5 +1,15 @@
 <?php require(VIEW_ROOT . '../templates/header.php'); ?>
-	<?php if($animal): ?>
+	<?php if($animal): 
+			if(isset($errors) && count($errors) > 0) {
+			echo "<div class=\"error\">";
+			echo "<b>The following errors occured:</b>";
+			echo "<ul>";
+			foreach ($errors as $error) {
+				echo "<li>$error</li>";
+			}
+			echo "</ul>";
+			echo "</div>";
+		}	?>
 	<h2>Edit <?php echo escape($animal['name']); ?></h2>
 
 	<form action="edit.php" enctype="multipart/form-data" method="POST" autocomplete="off">
@@ -17,17 +27,18 @@
 		</label>			
 		<label for="breed" class="bold"> 
 			Breed
-			<input type="text" name="breed" id="breed" value="<?php echo escape($animal['breed']); ?>" required/>
+			<input type="text" name="breed" id="breed" value="<?php echo escape($animal['breed']); ?>" />
 		</label>		
 		<label for="desc" class="bold"> 
 			Description
 			<textarea name="desc" id="desc" cols="30" rows="10"/ required><?php echo escape($animal['description']); ?></textarea>
 		</label>
 		<label for="photo" class="bold"> 
-			Photo <span class="small">(only jpg/png/gif are allowed - please note that the old photo will be lost)</span>
+			Photo <span class="small">(only jpg/png/gif are allowed - please note that the old photo will be lost if you choose to upload a new one)</span>
 			<input type="file" name="photo" id="photo" />
 		</label>	
 		<input type="hidden" name="submitted" value="true"/>		
+		<input type="hidden" name="id" value="<?php echo $animal['animalID']; ?>"/>	
 		<input type="submit" value="Submit"/>	
 	</form>
 	<?php else: ?>
