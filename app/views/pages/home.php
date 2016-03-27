58be6bfa6885bf2b33316ce1a42c9a614f31fac7
Denver Fernandes <sub367@gmail.com>
2016-03-27 19:42:06 +0100

    Change front page to display animals owned and adoption request status

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -1,5 +1,55 @@
 <?php require(VIEW_ROOT . '../templates/header.php'); ?>
-	
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/stupidtable.js"></script>
+<script type="text/javascript">    
+	$(function(){
+	        $("table").stupidtable();
+	    });
+</script>	
 	<p>Hello and welcome to the animal sanctuary website.</p>
+	<?php if($owns_assoc): ?>
+		<p>Below are all the animals you own:</p>
+			<ul>
+				<?php
+					foreach ($owns_assoc as $animal) {
+						echo "<li><a href=\"view.php?id=".$animal['animalID']."\">".$animal['name']."</a></li>";
+					}				
+				?>
+			</ul>
+	<?php else: ?>
+		<p>You have no owned animals.</p>
+	<?php endif; ?>
+
+	<?php if($requests): ?>
+		<p>Below are all the adoption requests you have submitted:</p>
+			<table class="center simpleTable">
+			<thead>
+				<tr>
+					<th data-sort="string">Name</th>
+					<th data-sort="string">Approval Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($requests_assoc as $request): ?>
+					<tr>
+						<td><a href="view.php?id="<?php echo $request['animalID'];?>><?php echo $request['name'];?></a></td>
+						<td><?php 
+						if($request['approved'] == 1) {
+							echo "Approved";
+						} else if($request['approved'] == 2) {
+							echo "Denied";
+						} else {
+							echo "Unchecked";
+						}
+						?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+			</table>
+		<?php else: ?>
+			<p>You have no adoption requests.</p>
+	<?php endif; ?>			
+
 
 <?php require(VIEW_ROOT . '../templates/footer.php'); ?>
--- index.php
+++ index.php
@@ -6,4 +6,29 @@ if(!isset($_SESSION["id"])) {
 	exit();
 }
 $title = "Home";
+
+$owns = $db->prepare("
+	SELECT * FROM owns
+	LEFT JOIN animal
+	ON owns.animalID = animal.animalID
+	WHERE owns.userID = :uid
+");
+
+$owns->execute(['uid' => $_SESSION["id"]]);
+
+$owns_assoc = $owns->fetchAll(PDO::FETCH_ASSOC);
+
+$requests = $db->prepare("
+	SELECT * FROM adoptionrequest
+	LEFT JOIN animal
+	ON adoptionrequest.animalID = animal.animalID
+	WHERE adoptionrequest.userID = :uid 
+");
+
+$requests->execute(['uid' => $_SESSION["id"]]);
+
+$requests_assoc = $requests->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($requests_assoc);
+
 require VIEW_ROOT . 'home.php';
